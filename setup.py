8ff7ed0573e30e0cb54efb0056dcc4f6a4565fcf
Sean O'Connor <sean.b.oconnor@gmail.com>
2009-08-01 09:21:49 -0400

    Incremented the version number in setup.py.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-cnote',
-    version='0.1.0',
+    version='0.2.0',
     description='Django cnotes provides a simple cookie based user notification system.',
     author="Sean O'Connor",
     author_email='sean.b.oconnor@gmail.com',
