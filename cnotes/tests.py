ddffae49a2788823425b30b10170d2c665e21b26
Sean O'Connor <sean.b.oconnor@gmail.com>
2009-08-01 10:14:38 -0400

    Added more unit tests.

--- cnotes/tests.py
+++ cnotes/tests.py
@@ -8,6 +8,7 @@ except ImportError:
     from md5 import new as sha1
     
 import cPickle as Pickle
+from Cookie import Morsel
 
 from django.test import TestCase
 from django.http import HttpRequest, HttpResponse
@@ -23,14 +24,23 @@ class CnotesTests(TestCase):
     def setUp(self):
         self.clear_text = 'Test Message'
         data = [self.clear_text]
-        encoded = base64.urlsafe_b64encode(Pickle.dumps(data))
         self.key = 'cnotes'
-        digest =  hmac.new(settings.SECRET_KEY, ':'.join([self.key, encoded]), sha1).hexdigest()
-        self.signed = '%s:%s' % (digest, encoded)
+        self.signed = self._sign(self.key, data)
+        self.empty_signed = self._sign(self.key, [])
         
+    def _sign(self, key, data):
+        encoded = base64.urlsafe_b64encode(Pickle.dumps(data))
+        
+        digest =  hmac.new(settings.SECRET_KEY, ':'.join([self.key, encoded]), sha1).hexdigest()
+        return '%s:%s' % (digest, encoded)
     
     def _get_no_cookie_request(self):
         return HttpRequest()
+        
+    def _get_simple_cookie_request(self):
+        request = HttpRequest()
+        request.COOKIES['cnotes'] = self.signed
+        return request
         
     def _get_response(self):
         return HttpResponse()
@@ -54,4 +64,29 @@ class CnotesTests(TestCase):
         # FIXME: Add a cookie morsel as the compairson
         self.assertEquals(response.cookies['cnotes'].key, expected_key)
         self.assertEquals(response.cookies['cnotes'].value, expected_value)
+        
+    def testRequestCnotes(self):
+        mware = CnotesHandlerMiddleware()
+        request = self._get_simple_cookie_request()
+        mware.process_request(request)
+        self.assertEquals(request.cnotes, [self.clear_text])
+        
+    def testGetCnotes(self):
+        import cnotes
+        mware = CnotesHandlerMiddleware()
+        request = self._get_simple_cookie_request()
+        mware.process_request(request)
+        self.assertEquals(cnotes.get(), [self.clear_text])
+        
+    def testGetAndClearCnotes(self):
+        import cnotes
+        mware = CnotesHandlerMiddleware()
+        request = self._get_simple_cookie_request()
+        mware.process_request(request)
+        self.assertEquals(cnotes.get_and_clear(), [self.clear_text])
+        self.assertEquals(cnotes.get_and_clear(), [])
+        orig_response = self._get_response()
+        response = mware.process_response(request, orig_response)
+        self.assertEquals(response.cookies[self.key].key, self.key)
+        self.assertEquals(response.cookies[self.key].value, self.empty_signed)
         
