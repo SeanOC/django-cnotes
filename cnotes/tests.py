dc0ef7c18742e670c32b77e3aee719cc9a263e42
Sean O'Connor <sean.b.oconnor@gmail.com>
2009-08-01 09:55:38 -0400

    Made unit tests more flexible.

--- cnotes/tests.py
+++ cnotes/tests.py
@@ -1,9 +1,33 @@
+import hmac
+import base64
+
+try:
+    from hashlib import sha1
+except ImportError:
+    # Compatibility with older versions of Python
+    from md5 import new as sha1
+    
+import cPickle as Pickle
+
 from django.test import TestCase
 from django.http import HttpRequest, HttpResponse
+from django.conf import settings
+
 
 from cnotes.middleware import CnotesHandlerMiddleware
 
+
 class CnotesTests(TestCase):
+    
+    
+    def setUp(self):
+        self.clear_text = 'Test Message'
+        data = [self.clear_text]
+        encoded = base64.urlsafe_b64encode(Pickle.dumps(data))
+        self.key = 'cnotes'
+        digest =  hmac.new(settings.SECRET_KEY, ':'.join([self.key, encoded]), sha1).hexdigest()
+        self.signed = '%s:%s' % (digest, encoded)
+        
     
     def _get_no_cookie_request(self):
         return HttpRequest()
@@ -20,13 +44,13 @@ class CnotesTests(TestCase):
         
     def testAddCnote(self):
         import cnotes
-        cnotes.add('Test Message')
+        cnotes.add(self.clear_text)
         mware = CnotesHandlerMiddleware()
         request = self._get_no_cookie_request()
         orig_response = self._get_response()
         response = mware.process_response(request, orig_response)
-        expected_key = "cnotes"
-        expected_value =  "715ea66a12e8953126e44e0de53d8c5f840a63ae:KGxwMQpTJ1Rlc3QgTWVzc2FnZScKcDIKYS4="
+        expected_key = self.key
+        expected_value =  self.signed
         # FIXME: Add a cookie morsel as the compairson
         self.assertEquals(response.cookies['cnotes'].key, expected_key)
         self.assertEquals(response.cookies['cnotes'].value, expected_value)
